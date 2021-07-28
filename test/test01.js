4118266454441291127bf1c3c04bbeffc46094bd
jeromflo <jeromflo@gmail.com>
2021-07-28 16:35:13 +0200

    Update test01.js
    terminado el ejercicio numero uno, iterador que muestra geeks o hubs segun sea multiplo de 3 o 5 respectivamente, se ha dedicado alrededor de 5 minutos

--- test/test01.js
+++ test/test01.js
@@ -1,4 +1,17 @@
 function geekshubs(index) {
-    return "";
+    var finalText="Iterador "+index;
+   for (var  i = 1; i <= index; i++) {
+      if(i%3==0){
+           finalText+="\nGeeks"
+      }else if(i%5==0){
+          finalText+="\nHubs"
+      }else{
+          finalText+="\n"+i;
+      }
+       
+   }
+
+
+   return finalText;
 }
 module.exports = geekshubs;
